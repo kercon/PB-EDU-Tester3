faba081bbbedf33f55b1d0d45330c433a774cabe
Ernest Czajkowski <kercondark@gmail.com>
2022-05-07 20:31:46 +0200

    extended sample

--- cypress/integration/e2e/basics6.spec.js
+++ cypress/integration/e2e/basics6.spec.js
@@ -4,5 +4,7 @@ describe('PersonalPage', function () {
     it('Should be able to visit', function () {
         cy.visit("http://127.0.0.1:8080/");
         cy.viewport(1920,1080);
+        cy.get(".testinput").type('test');
+        cy.get('#demo2').should("contain",'test');
     })
 })
